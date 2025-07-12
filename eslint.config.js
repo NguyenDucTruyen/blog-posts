14060d2a729788dae6af0ec25700d732340d04cc
TruyenBD <mrtruyenbd1407@gmail.com>
2025-07-12 14:16:13 +0700

    init

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettierConfig,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      // Prettier handles formatting, so disable conflicting ESLint rules
+      'no-trailing-spaces': 'off',
+      'eol-last': 'off',
+      'comma-dangle': 'off',
+      'max-len': 'off',
+      'object-curly-spacing': 'off',
+      quotes: 'off',
+      semi: 'off',
+      indent: 'off',
+    },
+  },
+]);
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-center text-blue-500'>
+        Hello world
+      </h1>
+      <div className='flex justify-center'>
+        <Button>Shadcn UI</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
